8bb22ae46a10c65bc249a6d08e3c9cf35d9d08e6
Volodymyr <klymenkodp@gmail.com>
2019-09-17 23:57:01 -0400

    Add colours

---
+++ ShopifyChallenge/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  ShopifyChallenge
+//
+//  Created by Volodymyr Klymenko on 2019-09-17.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let purple = UIColor(red: 117/255.0, green: 25/255.0, blue: 240/255.0, alpha: 1.0)
+    static let darkPurple = UIColor(red: 74/255.0, green: 22/255.0, blue: 119/255.0, alpha: 1.0)
+}
