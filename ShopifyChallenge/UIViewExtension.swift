ff8398b661739e854f7cb5ce8a8eecf27ef9690d
Volodymyr <klymenkodp@gmail.com>
2019-09-22 18:15:17 -0400

    Extract UIView extension

---
+++ ShopifyChallenge/UIViewExtension.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewExtension.swift
+//  ShopifyChallenge
+//
+//  Created by Volodymyr Klymenko on 2019-09-22.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
